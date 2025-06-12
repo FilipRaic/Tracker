d464839f61b520aa508e4d1a743bde255137cd34
AdrianVucko <adrian.viktorija@gmail.com>
2025-06-12 18:36:05 +0200

    HT-18: Added missing user conections in DB

--- src/main/resources/db/migration/V1/V1_0_6__user_schema.sql
+++ src/main/resources/db/migration/V1/V1_0_6__user_schema.sql
@@ -33,3 +33,19 @@ ALTER TABLE IF EXISTS daily_check
     ADD CONSTRAINT FK_daily_check_user FOREIGN KEY (user_id) REFERENCES application_user;
 
 CREATE INDEX IF NOT EXISTS IX_daily_check_user ON daily_check (user_id);
+
+ALTER TABLE IF EXISTS habit
+    ADD COLUMN IF NOT EXISTS user_id BIGINT NOT NULL DEFAULT 0;
+
+ALTER TABLE IF EXISTS habit
+    ADD CONSTRAINT FK_habit_user FOREIGN KEY (user_id) REFERENCES application_user;
+
+CREATE INDEX IF NOT EXISTS IX_habit_user ON habit (user_id);
+
+ALTER TABLE IF EXISTS journal_entry
+    ADD COLUMN IF NOT EXISTS user_id BIGINT NOT NULL DEFAULT 0;
+
+ALTER TABLE IF EXISTS journal_entry
+    ADD CONSTRAINT FK_journal_entry_user FOREIGN KEY (user_id) REFERENCES application_user;
+
+CREATE INDEX IF NOT EXISTS IX_journal_entry_user ON journal_entry (user_id);
